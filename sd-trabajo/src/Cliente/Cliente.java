de577ff44c5579e9b0d601af45b0b68a06e1655d
Ramón Sieira Martínez <rasieira@unirioja.es>
2019-12-02 21:22:38 +0100

    okey

--- sd-trabajo/src/Cliente/Cliente.java
+++ sd-trabajo/src/Cliente/Cliente.java
@@ -9,7 +9,6 @@ import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import Respositorios.Repositorio;
--- sd-trabajo/src/Principal/Principal.java
+++ sd-trabajo/src/Principal/Principal.java
@@ -5,10 +5,11 @@ import Cliente.Cliente;
 public class Principal {
 
 	public static void main(String[] args) {
-		int puerto=Integer.parseInt(args[1]);
-		Cliente c=new Cliente(args[0],puerto);
+		int puerto=Integer.parseInt("6666");
+		Cliente c=new Cliente("localhost",puerto);
 		String repositorio="prueba";
-		c.añadir(repositorio);
+		//c.añadir(repositorio);
+		c.clonar(repositorio);
 
 	}
 
