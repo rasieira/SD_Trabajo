7855b1bcb98ba85173110baca5ed1f03d271f7d8
RAMON SIEIRA MARTINEZ <rasieira@unirioja.es>
2019-12-29 15:23:03 +0100

    prueba 3

--- sd-trabajo/src/Principal/Principal.java
+++ sd-trabajo/src/Principal/Principal.java
@@ -24,30 +24,35 @@ public class Principal {
 				System.out.println("Introduce el nombre del Repositorio que quieres clonar");
 				Scanner sc2 = new Scanner(System.in);
 				String nombreRepo = sc2.nextLine();
+				nombreRepo.replace(" ","");
 				Cliente.clonar(nombreRepo);
 				break;
 			case "2":
 				System.out.println("Introduce el nombre del Repositorio que quieres subir");
 				Scanner sc21 = new Scanner(System.in);
 				String nombreRepo1 = sc21.nextLine();
+				nombreRepo1.replace(" ","");
 				Cliente.push(nombreRepo1);
 				break;
 			case "3":
 				System.out.println("Introduce el nombre del Repositorio que quieres traerte");
 				Scanner sc211 = new Scanner(System.in);
 				String nombreRepo11 = sc211.nextLine();
+				nombreRepo11.replace(" ","");
 				Cliente.pull(nombreRepo11);
 				break;
 			case "4":
 				System.out.println("Introduce el nombre del Repositorio que quieres eliminar");
 				Scanner sc2111 = new Scanner(System.in);
 				String nombreRepo111 = sc2111.nextLine();
+				nombreRepo111.replace(" ","");
 				Cliente.eliminar(nombreRepo111);
 				break;
 			case "5":
 				System.out.println("Introduce el nombre del Repositorio que quieres crear");
 				Scanner sc21111 = new Scanner(System.in);
 				String nombreRepo1111 = sc21111.nextLine();
+				nombreRepo1111.replace(" ","");
 				Cliente.anadir(nombreRepo1111);
 				break;
 			case "6":
